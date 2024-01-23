78c712c674fda5d1130e03896ab0db95357a8c05
Tech-OnLive <154456338+Tech-OnLive@users.noreply.github.com>
2024-01-23 03:52:22 +0100

    Fonction.py

--- Fonction.py
+++ Fonction.py
@@ -1,9 +1,24 @@
+import discord
+import json
+from discord.ext import commands
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from pytz import timezone
+from datetime import time
+
+intents = discord.Intents.default()
+intents.messages = True
+intents.guilds = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
 async def on_reaction_add(reaction, user):
     required_role_name = "DIGGERS/LFDM"
 
     if isinstance(user, discord.Member):
         if any(role.name == required_role_name for role in user.roles):
-            if reaction.emoji == "📌" and user != client.user:
+            if reaction.emoji == "📌":
                 message = reaction.message
                 message_data = {
                     'content': message.content
@@ -14,27 +29,42 @@ async def on_reaction_add(reaction, user):
                     json.dump(message_data, f)
                     f.write('\n')
 
-async def on_message(message):
+@bot.command(name='auj')
+async def auj(ctx):
     required_role_name = "DIGGERS/LFDM"
 
     # Vérifie si l'auteur du message a le rôle requis
-    if isinstance(message.author, discord.Member):
-        if any(role.name == required_role_name for role in message.author.roles):
-            if message.content == '!auj':
-                try:
-                    # Ouvre et lit le fichier des message enregistrés
-                    with open('MessageDuJour.json', 'r') as f:
-                        response = ''
-                        for line in f:
-                            message_data = json.loads(line)
-                            response += f"{message_data['content']}\n"
+    if any(role.name == required_role_name for role in ctx.author.roles):
+        try:
+            # Ouvre et lit le fichier des message enregistrés
+            with open('MessageDuJour.json', 'r') as f:
+                response = ''
+                for line in f:
+                    message_data = json.loads(line)
+                    response += f"{message_data['content']}\n"
 
-                    # Envoie un message avec tous les messages enregistrés
-                    if response:
-                        await message.channel.send(response)
-                    else:
-                        await message.channel.send("Pas de message enregistré aujourd'hui.")
-                except FileNotFoundError:
-                    await message.channel.send("Pas de fichier avec les messages.")
-                except Exception as e:
-                    await message.channel.send(f"Erreur: {e}")  
+            # Envoie un message avec tous les messages enregistrés
+            if response:
+                await ctx.send(response)
+            else:
+                await ctx.send("Pas de message enregistré aujourd'hui.")
+        except FileNotFoundError:
+            await ctx.send("Pas de fichier avec les messages.")
+        except Exception as e:
+            await ctx.send(f"Erreur: {e}")
+
+scheduler = AsyncIOScheduler(timezone="Europe/Paris")
+
+def clear_json():
+    try:
+        with open('MessageDuJour.json', 'w') as f:
+            f.truncate(0)
+        print("Fichier JSON vidé.")
+    except Exception as e:
+        print(f"Erreur lors de la vidange du fichier JSON: {e}")
+
+scheduler.add_job(clear_json, 'cron', hour=3, minute=47)
+
+scheduler.start()
+
+bot.run('MTE5NTc4MzQ1MTg0NTA4MzIzNw.GhfDcY.JXzP-2etQTteiD5Chci4_-ePnQmiSaNXpMjyg8')
